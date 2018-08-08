138c7b34fc2bdf9f4786b201933d6e4a7f401f34
James Lotruglio <jimlotruglio@yahoo.com>
2018-08-08 03:49:34 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,11 +2,11 @@ class Application < Sinatra::Base
   # Write your code here!
 get '/' do
   erb :index
-end 
+end
 
-post '/greet' do 
-  erb :greet 
-end 
+post '/greet' do
+  erb :greet
+end
 
 
 end
